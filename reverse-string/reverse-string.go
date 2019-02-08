1eee7d0f10bc36dc223631138b16571573f23d96
Arianna Vespri <arianna.vespri@yahoo.it>
2019-02-08 16:49:12 +0100

    Final version of reverse

--- reverse-string/reverse-string.go
+++ reverse-string/reverse-string.go
@@ -1,11 +1,23 @@
 package reverse
 
-func String(initial string) string {
-  reversed := []byte{}
+import (
+	"fmt"
+	"strings"
+	"unicode/utf8"
+)
 
-  for i := len(initial) -1 ; i >= 0; i-- {
-    v := initial[i]
-    reversed = append(reversed, v)
-  }
-    return string(reversed)
+//String reverses a string even if it's multi byte
+func String(initial string) string {
+	splitInitial := strings.Split(initial, "")
+	reversed := []string{}
+	for i := len(splitInitial) - 1; i >= 0; i-- {
+		v := splitInitial[i]
+		fmt.Printf("")
+		for len(v) > 0 {
+			r, size := utf8.DecodeRuneInString(v)
+			v = v[size:]
+			reversed = append(reversed, string(r))
+		}
+	}
+	return strings.Join(reversed, "")
 }
